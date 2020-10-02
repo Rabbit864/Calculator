e14f5fbe9c9aea053342e609b1af39226181819c
Daniil Smyshlyaev <smyshlyeavar@gmail.com>
2020-10-02 20:07:33 +0300

    fix: changed double quotes to single quotes

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 function calculator(value){
-  if(typeof value !== "string"){
-    return "Не строка";
+  if(typeof value !== 'string'){
+    return 'Не строка';
   }
   let signs = value.match(/[+*/]|-(?!\d)|\((?!-\d+\))|(?<!\(-\d+)\)|(?<!\()-/g);
   let numbers = value.match(/\d+|(?<=\()\-?\d+(?=\))/g);
   let character = value.match(/[^0-9\+\-\*\/|(|)]/g);
 
   if(signs === null || numbers === null || numbers.length > 5 || character !== null){
-    return "Введенно не выражение";
+    return 'Введенно не выражение';
   }
 
-  for(i = signs.indexOf("(")+1; signs.indexOf("(") !== -1 && signs.indexOf(")") !== -1; i++){
+  for(i = signs.indexOf('(')+1; signs.indexOf('(') !== -1 && signs.indexOf(')') !== -1; i++){
     
-    if(signs[i] !== ")"){
-      if(signs[i] === "*" || signs[i] === "/"){
+    if(signs[i] !== ')'){
+      if(signs[i] === '*' || signs[i] === '/'){
         let newNumber = calculateNumbers(numbers[i-1],numbers[i],signs[i]);
-        if(newNumber === "Деление на 0"){
-          return "Деление на 0";
+        if(newNumber === 'Деление на 0'){
+          return 'Деление на 0';
         }
         
         signs.splice(i,1);
@@ -26,11 +26,11 @@ function calculator(value){
       }
       
     }
-    if(signs[i] !== ")" && (signs[i+1] !== "*" && signs[i+1] !== "/") && (signs[i-1] !== "*" && signs[i-1] !== "/")){
-      if(signs[i] === "+" || signs[i] === "-"){
+    if(signs[i] !== ')' && (signs[i+1] !== '*' && signs[i+1] !== '/') && (signs[i-1] !== '*' && signs[i-1] !== '/')){
+      if(signs[i] === '+' || signs[i] === '-'){
         let newNumber = calculateNumbers(numbers[i-1],numbers[i],signs[i]);
-        if(newNumber == "Деление на 0"){
-          return "Деление на 0";
+        if(newNumber == 'Деление на 0'){
+          return 'Деление на 0';
         }
         signs.splice(i,1);
         
@@ -39,18 +39,18 @@ function calculator(value){
         i--;
       }
     }
-    if(signs[i] === ")" && signs[i-1] === "("){
+    if(signs[i] === ')' && signs[i-1] === '('){
       signs.splice(i,1);
       signs.splice(i-1,1);
     }
     
   }
-  for(i = 0; signs.indexOf("*") !== -1 || signs.indexOf("/") !== -1; i++){
+  for(i = 0; signs.indexOf('*') !== -1 || signs.indexOf('/') !== -1; i++){
 
-    if(signs[i] == "*" || signs[i] == "/"){
+    if(signs[i] == '*' || signs[i] == '/'){
       let newNumber = calculateNumbers(numbers[i],numbers[i+1],signs[i]);
-      if(newNumber == "Деление на 0"){
-        return "Деление на 0";
+      if(newNumber == 'Деление на 0'){
+        return 'Деление на 0';
       }
       signs.splice(i,1);
       numbers[i] = newNumber;
@@ -58,7 +58,7 @@ function calculator(value){
       i--;
     }
   }
-  for(i = 0; signs.indexOf("+") !== -1 || signs.indexOf("-") !== -1;){
+  for(i = 0; signs.indexOf('+') !== -1 || signs.indexOf('-') !== -1;){
     let newNumber = calculateNumbers(numbers[i],numbers[i+1],signs[i]);
     signs.splice(i,1);
     numbers[i] = newNumber;
@@ -69,25 +69,25 @@ function calculator(value){
 }
 function calculateNumbers(number1, number2, action) {
   switch (action) {
-    case "+":
+    case '+':
     return +number1 + +number2;
-    case "-":
+    case '-':
     return +number1 - +number2;
-    case "*":
+    case '*':
     return +number1 * +number2;
-    case "/":
-    if (number2 !== "0") {
+    case '/':
+    if (number2 !== '0') {
       return +number1 / +number2;
     } else {
-      return "Деление на 0";
+      return 'Деление на 0';
     }
   }
 }
 
-console.log(calculator("2*2"));
-console.log(calculator("2-2"));
-console.log(calculator("2/2"));
-console.log(calculator("2+2"));
-console.log(calculator("2/0"));
-console.log(calculator("2+(2-2*2)"));
-console.log(calculator("2+(2-2*2*2)"));
+console.log(calculator('2*2'));
+console.log(calculator('2-2'));
+console.log(calculator('2/2'));
+console.log(calculator('2+2'));
+console.log(calculator('2/0'));
+console.log(calculator('2+(2-2*2)'));
+console.log(calculator('2+(2-2*2*2)'));
